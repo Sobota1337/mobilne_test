b5d610480e48753c964ea52881d8656c2b6c9e48
kerty <kertycorp@gmail.com>
2021-10-28 13:00:44 +0200

    tt

--- app/src/main/java/com/example/z/MainActivity.kt
+++ app/src/main/java/com/example/z/MainActivity.kt
@@ -11,15 +11,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val replace = findViewById<Button>(R.id.button);
-        val add = findViewById<Button>(R.id.button2);
-        val suma = findViewById<Button>(R.id.button3);
+        val replace = findViewById<Button>(R.id.b_replace);
+        val add = findViewById<Button>(R.id.b_add);
+        val suma = findViewById<Button>(R.id.b_suma);
 
-        val x1 = findViewById<EditText>(R.id.editTextNumber);
-        val x2 = findViewById<EditText>(R.id.editTextNumber2);
+        val x1 = findViewById<EditText>(R.id.x1);
+        val x2 = findViewById<EditText>(R.id.x2);
 
-        val input = findViewById<EditText>(R.id.editTextTextPersonName);
-        val output = findViewById<TextView>(R.id.textView3);
+        val input = findViewById<EditText>(R.id.i_textbox);
+        val output = findViewById<TextView>(R.id.t_output);
 
 
         replace.setOnClickListener {
